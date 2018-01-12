fe831ea5dc58c0099a05bc92d915f0eed50b5cff
pyfection <mat@pyfection.com>
2018-01-12 20:18:24 +0100

    ensured that troops are always drawn on top of tiles

--- core/actor/visual/view.py
+++ core/actor/visual/view.py
@@ -17,6 +17,7 @@ class View(Widget):
         self._keyboard = Window.request_keyboard(
             self._keyboard_closed, self, 'text')
         self._keyboard.bind(on_key_down=self._on_keyboard_down)
+        self.troops = {}
 
     def _keyboard_closed(self):
         self._keyboard.unbind(on_key_down=self._on_keyboard_down)
@@ -39,7 +40,11 @@ class View(Widget):
             pos=(c_tile.x, c_tile.y),
         )
         self.ids.map.add_widget(tile)
+        for troop in self.troops.values():  # ToDo: this is not very performant, find a better solution
+            self.ids.map.remove_widget(troop)
+            self.ids.map.add_widget(troop)
 
     def add_troop(self, c_troop):
         troop = assets.Troop((c_troop.x, c_troop.y))
         self.ids.map.add_widget(troop)
+        self.troops[c_troop.id] = troop
