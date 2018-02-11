cf7aa96b2dd7cd47289dfdca253a68ba438a07d2
pyfection <mat@pyfection.com>
2018-02-11 13:58:12 +0100

    added very basic game setup screen

--- main.py
+++ main.py
@@ -4,18 +4,22 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager
 
 from visual.main_menu import MainMenu
+from visual.game_setup import GameSetup
 
 
 class GameApp(App):
     title = "Strategos"
     screenmanager = None
     main_menu = None
+    game_setup = None
 
     def build(self):
         self.screenmanager = ScreenManager()
         self.main_menu = MainMenu()
+        self.game_setup = GameSetup()
 
         self.screenmanager.add_widget(self.main_menu)
+        self.screenmanager.add_widget(self.game_setup)
         return self.screenmanager
 
 
--- visual/game_setup.py
+++ visual/game_setup.py
@@ -6,8 +6,8 @@ from kivy.lang.builder import Builder
 from kivy.uix.screenmanager import Screen
 
 
-class MainMenu(Screen):
+class GameSetup(Screen):
     def __init__(self):
-        kv_path = os.path.join(os.path.dirname(__file__), 'main_menu.kv')
+        kv_path = os.path.join(os.path.dirname(__file__), 'game_setup.kv')
         Builder.load_file(kv_path)
-        super().__init__(name='main_menu')
+        super().__init__(name='game_setup')
