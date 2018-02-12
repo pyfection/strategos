89f9d3160546c02e70ea7ffe31d5985bd079d7c6
pyfection <mat@pyfection.com>
2018-02-12 21:28:09 +0100

    extended game setup screen

--- visual/game_setup.py
+++ visual/game_setup.py
@@ -4,6 +4,8 @@ import os
 
 from kivy.lang.builder import Builder
 from kivy.uix.screenmanager import Screen
+from kivy.uix.label import Label
+from kivy.uix.button import Button
 
 
 class GameSetup(Screen):
@@ -11,3 +13,13 @@ class GameSetup(Screen):
         kv_path = os.path.join(os.path.dirname(__file__), 'game_setup.kv')
         Builder.load_file(kv_path)
         super().__init__(name='game_setup')
+        self.players = {}
+
+        self.add_player('test_player')
+
+    def add_player(self, name):
+        self.players[name] = {}
+        name = Label(text=name)
+        options = Button(text="Options")
+        self.ids.players.add_widget(name)
+        self.ids.players.add_widget(options)
